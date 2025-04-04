be70b0d9ac2c4517fa85d3e81d2836f72334425e
Muhammad Hamza <hamzaadil56@gmail.com>
2025-04-04 22:54:52 +0500

    added facebook create post function.

--- src/openai_agents_apps/smm_agent/custom_tools.py
+++ src/openai_agents_apps/smm_agent/custom_tools.py
@@ -4,6 +4,7 @@ from typing_extensions import TypedDict, Any
 
 from agents import RunContextWrapper, function_tool
 from langchain_tavily import TavilySearch
+import requests
 
 
 @function_tool(name_override="research")
@@ -18,3 +19,25 @@ async def search_internet(ctx: RunContextWrapper[Any], query: str) -> str:
         topic="general",
     )
     return tool.invoke({"query": query})
+
+
+@function_tool
+def post_to_facebook(page_id, access_token, message):
+    """
+    Posts a message to a Facebook page.
+
+    Parameters:
+    - page_id: str, the ID of the Facebook page.
+    - access_token: str, the long-lived Page Access Token.
+    - message: str, the content to post.
+
+    Returns:
+    - Response JSON from Facebook Graph API.
+    """
+    post_url = f"https://graph.facebook.com/v19.0/{page_id}/feed"
+    payload = {
+        'message': message,
+        'access_token': access_token
+    }
+    response = requests.post(post_url, data=payload)
+    return response.json()
